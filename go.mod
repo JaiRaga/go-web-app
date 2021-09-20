d29fb0c5949321147f81f6a52e7abc741f9100b8
JaiRaga <raga.j.santhosh@gmail.com>
2021-09-20 15:01:56 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module my-app-3
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const PORT = ":8080"
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println("Server if running on port", PORT)
+	_ = http.ListenAndServe(PORT, nil)
+}
